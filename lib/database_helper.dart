330c8ef3715fad7360c1274ea232b543ae5052b1
thiruna31 <plthirunavukkarasu01@gmail.com>
2025-10-16 20:58:31 -0400

    main dart

---
+++ lib/database_helper.dart
@@ -0,0 +1,83 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DatabaseHelper {
+  static const _databaseName = "MyDatabase.db";
+  static const _databaseVersion = 1;
+
+  static const table = 'my_table';
+  static const columnId = '_id';
+  static const columnName = 'Name';
+  static const columnAge = 'Age';
+
+  static Database? _db;
+
+  Future<Database> get database async {
+    if (_db != null) return _db!;
+    _db = await _initDatabase();
+    return _db!;
+  }
+
+  Future<Database> _initDatabase() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = join(directory.path, _databaseName);
+    return await openDatabase(
+      path,
+      version: _databaseVersion,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE $table (
+        $columnId INTEGER PRIMARY KEY AUTOINCREMENT,
+        $columnName TEXT NOT NULL,
+        $columnAge INTEGER NOT NULL
+      )
+    ''');
+  }
+
+  Future<int> insert(Map<String, dynamic> row) async {
+    final db = await database;
+    return await db.insert(table, row);
+  }
+
+  Future<List<Map<String, dynamic>>> queryAllRows() async {
+    final db = await database;
+    return await db.query(table);
+  }
+
+  Future<Map<String, dynamic>?> queryById(int id) async {
+    final db = await database;
+    final results = await db.query(
+      table,
+      where: '$columnId = ?',
+      whereArgs: [id],
+    );
+    return results.isNotEmpty ? results.first : null;
+  }
+
+  Future<int> queryRowCount() async {
+    final db = await database;
+    final results = await db.rawQuery('SELECT COUNT(*) FROM $table');
+    return Sqflite.firstIntValue(results) ?? 0;
+  }
+
+  Future<int> update(Map<String, dynamic> row) async {
+    final db = await database;
+    int id = row[columnId];
+    return await db.update(table, row, where: '$columnId = ?', whereArgs: [id]);
+  }
+
+  Future<int> delete(int id) async {
+    final db = await database;
+    return await db.delete(table, where: '$columnId = ?', whereArgs: [id]);
+  }
+
+  Future<int> deleteAll() async {
+    final db = await database;
+    return await db.delete(table);
+  }
+}
